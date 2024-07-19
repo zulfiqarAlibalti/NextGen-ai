a65d70bed36aa78f2f037efe48386316a334a85f
zulfiqarAlibalti <zulfibalti592@gmail.com>
2024-07-19 20:11:32 +0500

    layout error fixed

--- components/user-avatar.tsx
+++ components/user-avatar.tsx
@@ -1,23 +1,17 @@
 import { useUser } from "@clerk/nextjs";
-import { Avatar,
-     AvatarFallback,
-      AvatarImage 
-} from "@/components/ui/avatar";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 export const UserAvatar = () => {
-    const { user } = useUser();
+  const { user } = useUser();
 
-    const profileImageUrl = user?.profileImageUrl || "";
-    const firstNameInitial = user?.firstName?.charAt(0) || "";
-    const lastNameInitial = user?.lastName?.charAt(0) || "";
-
-    return (
-        <Avatar className="h-8 w-8">
-            <AvatarImage src={profileImageUrl} />
-            <AvatarFallback>
-                {firstNameInitial}
-                {lastNameInitial}
-            </AvatarFallback>
-        </Avatar>
-    );
+  return (
+    <Avatar className="h-8 w-8">
+      <AvatarImage src={user?.imageUrl} />
+      <AvatarFallback>
+        {user?.firstName?.charAt(0)}
+        {user?.lastName?.charAt(0)}
+      </AvatarFallback>
+    </Avatar>
+  );
 };
