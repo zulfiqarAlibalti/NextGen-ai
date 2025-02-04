e3a108810b3b95ff7b61bab46c3f29e7f1ce5a86
zulfiqarAlibalti <zulfibalti592@gmail.com>
2025-02-04 11:03:48 +0500

    fix database issues

--- app/api/stripe/route.ts
+++ app/api/stripe/route.ts
@@ -44,7 +44,7 @@ export async function GET() {
           price_data: {
             currency: "USD",
             product_data: {
-              name: "Genius Pro",
+              name: "NextGen AI Pro",
               description: "Unlimited AI Generations",
             },
             unit_amount: 2000,
